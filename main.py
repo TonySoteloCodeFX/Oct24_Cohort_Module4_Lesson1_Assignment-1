7173244b1f20fc18df38fef296c03d9e7f28dac9
TonySoteloCodeFX <tonysotelo.vfx@gmail.com>
2024-10-22 00:40:34 -0700

    Text Utils Complete

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import mood_responses
+import text_utils as u
 
 print("-" * 50)
+print("1. Python Modules and Data Handling Assignment\n")
 print("How are you feeling today?")
 print("Happy, Sad, or Mad")
 mood = input("Enter Mood: ").lower()
+print("")
 print(mood_responses.mood_response(mood))
+print("-" * 50)
+
+print("2. Mastering Python Modules and Aliases\n")
+s = "Hello, Welcome to the world of Python."
+print(u.revers_string(s))
+print("")
+print(u.capitalize_string(s))
 print("-" * 50)
--- mood_responses.py
+++ mood_responses.py
@@ -1,15 +1,11 @@
 def mood_response(mood):
     match mood:
         case "happy":
-            print("-" * 50)
             return "I'm glad you're having a great day!"
         case "sad":
-            print("-" * 50)
             return "I hope your day gets better."
         case "mad":
-            print("-" * 50)
             return "Count to 10 and take deep breaths. It will help."
         case _:
-            print("-" * 50)
             return "That is not an option. Try again!"
         
